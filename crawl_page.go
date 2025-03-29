7a3fae0e79168afdf4483949faab8cd7b33989fa
Elianamos <elianamos29@gmail.com>
2025-03-29 17:09:52 +0300

    max pages

--- crawl_page.go
+++ crawl_page.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 	"net/url"
-	"strings"
 	"sync"
 )
 
 type config struct {
 	pages              map[string]int
+	maxPages		   int
 	baseURL            *url.URL
 	mu                 *sync.Mutex
 	concurrencyControl chan struct{}
 	wg                 *sync.WaitGroup
 }
 
-func configure(rawBaseURL string, maxConcurrency int) (*config, error) {
+func configure(rawBaseURL string, maxConcurrency, maxPages int) (*config, error) {
 	baseURL, err := url.Parse(rawBaseURL)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
@@ -23,6 +23,7 @@ func configure(rawBaseURL string, maxConcurrency int) (*config, error) {
 
 	return &config{
 		pages:              make(map[string]int),
+		maxPages: 			maxPages,
 		baseURL:            baseURL,
 		mu:                 &sync.Mutex{},
 		concurrencyControl: make(chan struct{}, maxConcurrency),
@@ -36,6 +37,10 @@ func (cfg *config) crawlPage(rawCurrentURL string) {
 		<-cfg.concurrencyControl
 		cfg.wg.Done()
 	}()
+
+	if cfg.pageCount() >= cfg.maxPages {
+		return
+	}
 
 	currentURL, err := url.Parse(rawCurrentURL)
 	if err != nil {
@@ -77,23 +82,6 @@ func (cfg *config) crawlPage(rawCurrentURL string) {
 	}
 }
 
-func sameDomain(baseURL, refURL string) (bool, error) {
-	parsedBase, err := url.Parse(baseURL)
-	if err != nil {
-		return false, fmt.Errorf("couldn't parse url %s: %v", baseURL, err)
-	}
-
-	parsedRef, err := url.Parse(refURL)
-	if err != nil {
-		return false, fmt.Errorf("couldn't parse url %s: %v", refURL, err)
-	}
-
-	baseHost := strings.TrimPrefix(parsedBase.Host, "www.")
-	refHost := strings.TrimPrefix(parsedRef.Host, "www.")
-
-	return baseHost == refHost, nil
-}
-
 func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
 	cfg.mu.Lock()
 	defer cfg.mu.Unlock()
@@ -105,4 +93,10 @@ func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
 
 	cfg.pages[normalizedURL] = 1
 	return true
+}
+
+func (cfg *config) pageCount() int {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	return len(cfg.pages)
 }
--- main.go
+++ main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	args := os.Args
-	if len(args) != 2 {
-		if len(args) == 1 {
-			fmt.Println("no website provided")
-			os.Exit(1)
-		} else {
-			fmt.Println("too many arguments provided")
-			os.Exit(1)
-		}
+	if len(args) < 4 {
+		fmt.Println("not enough arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
+		return
+	}
+	if len(args) > 4 {
+		fmt.Println("too many arguments provided")
+		return
 	}
 
 	rawBaseURL := args[1]
+	maxConcurrency, err := strconv.Atoi(args[2])
+	if err != nil {
+		fmt.Printf("Error - invalid value for maxConcurrency: %v", err)
+		return
+	}
+	maxPages, err := strconv.Atoi(args[3])
+	if err != nil {
+		fmt.Printf("Error - invalid value for maxPages: %v", err)
+		return
+	}
 
-	const maxConcurrency = 3
-	cfg, err := configure(rawBaseURL, maxConcurrency)
+	cfg, err := configure(rawBaseURL, maxConcurrency, maxPages)
 	if err != nil {
 		fmt.Printf("Error - configure: %v", err)
 		return
